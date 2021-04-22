4d99592b746c4ba73e7fbc1ed32db4f939c510a3
Danilo <danilo@kuehns.club>
2021-04-22 21:44:00 +0200

    ADD clean to gradle build

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-docker run --rm -u gradle -v "$PWD":/home/gradle/project -w /home/gradle/project gradle:jdk16 gradle bootJar
+docker run --rm -u gradle -v "$PWD":/home/gradle/project -w /home/gradle/project gradle:jdk16 gradle clean bootJar
 docker build -t "mailgroup:latest" .
