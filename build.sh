bdbbf63d7467ca6b1bb16cdd423e2a23501db0c7
Danilo <danilo@kuehns.club>
2021-04-22 21:49:55 +0200

    clean build folder

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-docker run --rm -u gradle -v "$PWD":/home/gradle/project -w /home/gradle/project gradle:jdk16 gradle clean bootJar
+rm -fR build
+docker run --rm -u gradle -v "$PWD":/home/gradle/project -w /home/gradle/project gradle:jdk16 gradle bootJar
 docker build -t "mailgroup:latest" .
